db87bc675a67a431a8093d1fc1ce49284f765278
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-02-07 14:50:21 -0500

    fin.
    fin.

---
+++ page9.java
@@ -0,0 +1,14 @@
+/*****
+ * Project: page 9
+ * date: Feb. 7. 2024
+ * Program name: page9.jave
+ * Programmer: jacob Kim
+ **************/
+
+public class page9
+{
+    public static void main (String[] args){
+        System.out.println("Hi");
+        System.out.println("LabOneA");
+    }
+}
